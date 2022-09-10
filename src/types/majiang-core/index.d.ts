15248ea7e825d21117fe3e3e035c858b4906dcae
Tanimodori <unknowner2014@gmail.com>
2022-09-11 07:32:55 +0800

    types: rule

--- src/types/majiang-core/index.d.ts
+++ src/types/majiang-core/index.d.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="data.d.ts" />
+/// <reference path="rule.d.ts" />
 /// <reference path="shoupai.d.ts" />
 /// <reference path="shan.d.ts" />
 /// <reference path="he.d.ts" />
