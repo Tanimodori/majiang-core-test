a3cee839b4960eadb64ece93c437c639c4254bdd
Tanimodori <unknowner2014@gmail.com>
2022-09-11 08:05:05 +0800

    types: HuleResult

--- src/types/majiang-core/hule.d.ts
+++ src/types/majiang-core/hule.d.ts
@@ -4,16 +4,39 @@ declare module '@kobalab/majiang-core' {
      * ``shoupai`` の和了点を計算し、和了情報とともに返す。
      * @remarks
      * ツモ和了の場合は ``shoupai`` はツモ牌を加えた状態で ``rongpai`` は null とする。
-     * ロン和了の場合は ``shoupai`` はロン牌を加えない状態で ``rongpai`` はロンした [[牌]] とする。``rongpai`` には誰がロンしたかを示す ``+``(下家から和了)/``=``(対面から和了)/``-``(上家から和了) のフラグを付加する。
+     * ロン和了の場合は ``shoupai`` はロン牌を加えない状態で ``rongpai`` はロンした [[牌]] とする。``rongpai`` には誰がロンしたかを示す ``+``(下家から和了)/**=``(対面から和了)/**-``(上家から和了) のフラグを付加する。
      * @param shoupai 手牌
      * @param rongpai 牌
      * @param param 和了点計算に使用する場況情報
      * @returns 和了情報
      */
-    function hule(shoupai: Shoupai, rongpai: Pai, param: HuleParam): unknown;
+    function hule(shoupai: Shoupai, rongpai: Pai, param: HuleParam): HuleResult;
 
     /**
      * 和了点計算に使用する場況情報
+     * @example
+     * 以下の構造のオブジェクトである。
+     * ```JavaScript
+     * {
+     *     rule:           Majiang.rule(),
+     *     zhuangfeng:     0,
+     *     menfeng:        0,
+     *     hupai: {
+     *         lizhi:      0,
+     *         yifa:       false,
+     *         qianggang:  false,
+     *         lingshang:  false,
+     *         haidi:      0,
+     *         tianhu:     0
+     *     },
+     *     baopai:         [ 'm1' ],
+     *     fubaopai:       null,
+     *     jicun: {
+     *         changbang:  0,
+     *         lizhibang:  0
+     *     }
+     * }
+     * ```
      */
     interface HuleParam {
       /** 和了点計算時に使用するルール。 */
@@ -49,5 +72,54 @@ declare module '@kobalab/majiang-core' {
         lizhibang: number;
       };
     }
+  }
+
+  /**
+   * 和了情報
+   * @example
+   * 以下の構造のオブジェクトである。
+   * ```JavaScript
+   * {
+   *     hupai:      [ { name: "立直", fanshu: 1 },
+   *                   { name: "門前清自模和", fanshu: 1 },
+   *                   { name: "裏ドラ", fanshu: 1 } ],
+   *     fu:         40,
+   *     fanshu:     3,
+   *     damanguan:  null,
+   *     defen:      5200,
+   *     fenpei:     [ -2600, 6200, -1300, -1300 ]
+   * }
+   */
+  interface HuleResult {
+    /**
+     * 和了役の配列。それぞれの要素には役名を示す ``name`` と翻数を示す ``fanshu`` がある。
+     * 役満の場合 ``fanshu`` は数字ではなく、和了役それぞれの役満複合数分の ``*`` となる。
+     * また役満のパオがあった場合は ``baojia`` に責任者を設定する。
+     */
+    hupai: Huleyi[];
+    /**
+     * 符。役満の場合は ``undefined``。
+     */
+    fu?: number;
+    /**
+     * 翻数。役満の場合は ``undefined``。
+     */
+    fanshu?: number;
+    /**
+     * 役満複合数。
+     * 複合には四暗刻をダブル役満にする類のものと、大三元と字一色の複合のような役の複合のケースがある。
+     * 役満でない場合は ``undefined``。
+     */
+    damanguan?: number;
+    /**
+     * 和了打点。供託収入は含まない。
+     */
+    defen: number;
+    /**
+     * 供託を含めたその局の点数の収支。
+     * その局の東家から順に並べる。
+     * リーチ宣言による1000点減は収支に含めない。
+     */
+    fenpai: number[];
   }
 }
