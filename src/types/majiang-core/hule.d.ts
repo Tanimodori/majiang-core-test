70127ced1ffa0150024308c5e41f8ffcb4193cbd
Tanimodori <unknowner2014@gmail.com>
2022-09-13 10:10:17 +0800

    types: hule_mianzi path

--- src/types/majiang-core/hule.d.ts
+++ src/types/majiang-core/hule.d.ts
@@ -116,70 +116,70 @@ declare module '@kobalab/majiang-core' {
         lizhibang: number;
       };
     }
-  }
 
-  /**
-   * 和了情報
-   * @example
-   * 以下の構造のオブジェクトである。
-   * ```JavaScript
-   * {
-   *     hupai:      [ { name: "立直", fanshu: 1 },
-   *                   { name: "門前清自模和", fanshu: 1 },
-   *                   { name: "裏ドラ", fanshu: 1 } ],
-   *     fu:         40,
-   *     fanshu:     3,
-   *     damanguan:  null,
-   *     defen:      5200,
-   *     fenpei:     [ -2600, 6200, -1300, -1300 ]
-   * }
-   * ```
-   */
-  interface HuleResult {
     /**
-     * 和了役の配列。それぞれの要素には役名を示す **`name`** と翻数を示す **`fanshu`** がある。
-     * 役満の場合 **`fanshu`** は数字ではなく、和了役それぞれの役満複合数分の `*` となる。
-     * また役満のパオがあった場合は **`baojia`** に責任者を設定する。
+     * 和了情報
+     * @example
+     * 以下の構造のオブジェクトである。
+     * ```JavaScript
+     * {
+     *     hupai:      [ { name: "立直", fanshu: 1 },
+     *                   { name: "門前清自模和", fanshu: 1 },
+     *                   { name: "裏ドラ", fanshu: 1 } ],
+     *     fu:         40,
+     *     fanshu:     3,
+     *     damanguan:  null,
+     *     defen:      5200,
+     *     fenpei:     [ -2600, 6200, -1300, -1300 ]
+     * }
+     * ```
      */
-    hupai: Huleyi[];
+    interface HuleResult {
+      /**
+       * 和了役の配列。それぞれの要素には役名を示す **`name`** と翻数を示す **`fanshu`** がある。
+       * 役満の場合 **`fanshu`** は数字ではなく、和了役それぞれの役満複合数分の `*` となる。
+       * また役満のパオがあった場合は **`baojia`** に責任者を設定する。
+       */
+      hupai: Huleyi[];
+      /**
+       * 符。役満の場合は `undefined`。
+       */
+      fu?: number;
+      /**
+       * 翻数。役満の場合は `undefined`。
+       */
+      fanshu?: number;
+      /**
+       * 役満複合数。
+       * 複合には四暗刻をダブル役満にする類のものと、大三元と字一色の複合のような役の複合のケースがある。
+       * 役満でない場合は `undefined`。
+       */
+      damanguan?: number;
+      /**
+       * 和了打点。供託収入は含まない。
+       */
+      defen: number;
+      /**
+       * 供託を含めたその局の点数の収支。
+       * その局の東家から順に並べる。
+       * リーチ宣言による1000点減は収支に含めない。
+       */
+      fenpai: number[];
+    }
     /**
-     * 符。役満の場合は `undefined`。
+     * **`param`** で指定された値を元に {@link Util.hule} の第3パラメータに使用する場況情報を返す。
+     * @param param 指定された場況情報
+     * @returns 場況情報
+     * @see hule
      */
-    fu?: number;
-    /**
-     * 翻数。役満の場合は `undefined`。
-     */
-    fanshu?: number;
-    /**
-     * 役満複合数。
-     * 複合には四暗刻をダブル役満にする類のものと、大三元と字一色の複合のような役の複合のケースがある。
-     * 役満でない場合は `undefined`。
-     */
-    damanguan?: number;
-    /**
-     * 和了打点。供託収入は含まない。
-     */
-    defen: number;
-    /**
-     * 供託を含めたその局の点数の収支。
-     * その局の東家から順に並べる。
-     * リーチ宣言による1000点減は収支に含めない。
-     */
-    fenpai: number[];
-  }
-  /**
-   * **`param`** で指定された値を元に {@link Util.hule} の第3パラメータに使用する場況情報を返す。
-   * @param param 指定された場況情報
-   * @returns 場況情報
-   * @see hule
-   */
-  function hule_param(param: Partial<HuleParam> = {}): HuleParam;
+    function hule_param(param: Partial<HuleParam> = {}): HuleParam;
 
-  /**
-   * **`shoupai`** の手牌から **`rongpai`** で和了したときの和了形の一覧を返す。
-   * @param shoupai {@link Shoupai | 手牌}
-   * @param rongpai {@link Pai | 牌}
-   * @returns 和了形の配列。和了形にならない場合は空配列を返す。
-   */
-  function hule_mianzi(shoupai: Shoupai, rongpai: Pai): Paizi[];
+    /**
+     * **`shoupai`** の手牌から **`rongpai`** で和了したときの和了形の一覧を返す。
+     * @param shoupai {@link Shoupai | 手牌}
+     * @param rongpai {@link Pai | 牌}
+     * @returns 和了形の配列。和了形にならない場合は空配列を返す。
+     */
+    function hule_mianzi(shoupai: Shoupai, rongpai: Pai): Paizi[];
+  }
 }
